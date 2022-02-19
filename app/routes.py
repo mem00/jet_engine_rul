7bd245af74d0858ccd25cd42e5c3b059c8ba0dba
Michael McGuire <michael@mcguire.ch>
2022-02-19 11:18:19 -0500

    add lstm inference route

--- app/routes.py
+++ app/routes.py
@@ -1,5 +1,9 @@
-from flask import current_app as app
+from flask import current_app as app, render_template
+from .lstm_inference import predict_rul
+from flask import render_template
 
 @app.route("/")
-def hello_world():
-    return "<p>RUL app!</p>"
+def lstm_inference():
+    prediction = predict_rul()
+    print(prediction[0][0], "tavo")
+    return render_template("prediction.html", prediction=prediction[0][0])
