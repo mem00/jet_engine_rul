ef386b698924245262717767ae090cab71f87bac
Michael McGuire <michael@mcguire.ch>
2022-02-19 11:52:46 -0500

    add actual to prediction

--- app/routes.py
+++ app/routes.py
@@ -1,9 +1,10 @@
 from flask import current_app as app, render_template
 from .lstm_inference import predict_rul
 from flask import render_template
+import math
 
 @app.route("/")
 def lstm_inference():
-    prediction = predict_rul()
+    prediction, actual = predict_rul()
     print(prediction[0][0], "tavo")
-    return render_template("prediction.html", prediction=prediction[0][0])
+    return render_template("prediction.html", prediction=math.floor(prediction[0][0]), actual=actual)
